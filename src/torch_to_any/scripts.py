a03ee64edb67ab91c0bf83cb72ddf4123f22f33f
Dimda <dimidagd@gmail.com>
2025-05-28 17:32:41 +0200

    ✨ feat(scripts): enhance model conversion functions with ONNX and TorchScript exporters
    - replace TestModel with ImageClassifierModel for ONNX export
    - implement ONNXExporter and TorchScriptExporter for model conversion

--- src/torch_to_any/scripts.py
+++ src/torch_to_any/scripts.py
@@ -3,7 +3,12 @@
 # %% IMPORTS
 
 import sys
-from torch_to_any.modeling import ImageClassifierModel as Model
+
+from torch_to_any.cli import cli
+from torch_to_any.modeling import TestModel as Model
+from torch_to_any.ops.onnx import ONNXExporter
+from torch_to_any.ops.torchscript import TorchScriptExporter
+
 # %% FUNCTIONS
 
 
@@ -11,11 +16,22 @@ def main(argv: list[str] | None = None) -> int:
     """Run the main script function."""
     args = argv or sys.argv[1:]
     print("Args:", args)
+    cli()
     return 0
 
 
 def to_onnx(output_model: str) -> None:
     """Convert a PyTorch model to ONNX format."""
     torch_model = Model()
-    from onnx_ops import onnx_export_pipeline
-    onnx_export_pipeline(torch_model, output_model)
+    torch_model.eval()  # Set the model to evaluation mode
+
+    exporter = ONNXExporter()
+    exporter.export_pipeline(torch_model, output_model)
+
+
+def to_torchscript(output_model: str) -> None:
+    """Convert a PyTorch model to TorchScript format."""
+    torch_model = Model()
+    torch_model.eval()
+    exporter = TorchScriptExporter()
+    exporter.export_pipeline(torch_model, output_model)
