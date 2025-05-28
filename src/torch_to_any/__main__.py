92684124f2b442cbe2575c25596fe296a69755a0
Dimda <dimidagd@gmail.com>
2025-05-28 17:22:59 +0200

    remove CLI commands for ONNX and TorchScript conversion

--- src/torch_to_any/__main__.py
+++ src/torch_to_any/__main__.py
@@ -1,23 +1,6 @@
 """Entry point of the package."""
 
-import click
 from torch_to_any import scripts
 
-@click.group()
-def cli():
-    pass
-
-@cli.command()
-@click.argument("output_model")
-def to_onnx(output_model):
-    """Convert model to ONNX format."""
-    scripts.to_onnx(output_model)
-
-@cli.command()
-@click.argument("output_model")
-def to_torchscript(output_model):
-    """Convert model to TorchScript format."""
-    scripts.to_torchscript(output_model)
-
 if __name__ == "__main__":
-    cli()
+    scripts.main()
